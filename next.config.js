a95a7dc4620d0ecbb67c2151091ebfa4dcb17ac4
Léo Pradel <pradel.leo@gmail.com>
2023-06-23 17:36:31 +0200

    fix: fix lambda size

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const getVercelPreviewUrl = () => {
+  return `https://${
+    process.env.VERCEL_GIT_REPO_SLUG
+  }-git-${process.env.VERCEL_GIT_COMMIT_REF.replace(
+    new RegExp("/", "g"),
+    "-"
+  )}-${process.env.VERCEL_GIT_REPO_OWNER}.vercel.app`;
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  env: {
+    APP_URL: process.env.APP_URL || getVercelPreviewUrl(),
   },
 };
 
--- src/pages/api/twitter/[id].ts
+++ src/pages/api/twitter/[id].ts
@@ -1,4 +1,4 @@
-import { chromium as playwright } from "playwright-core";
+// import { chromium as playwright } from "playwright-core";
 import chromium from "@sparticuz/chromium";
 import type { NextApiHandler } from "next";
 
@@ -12,26 +12,26 @@ export const revalidate = 86400;
 const twitterEmbedEndpoint: NextApiHandler = async (req, res) => {
   const { id } = req.query;
 
-  const browser = await playwright.launch({
-    args: chromium.args,
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? await chromium.executablePath
-        : undefined,
-    headless: true,
-  });
+  // const browser = await playwright.launch({
+  //   args: chromium.args,
+  //   executablePath:
+  //     process.env.NODE_ENV === "production"
+  //       ? await chromium.executablePath
+  //       : undefined,
+  //   headless: true,
+  // });
 
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  // const context = await browser.newContext();
+  // const page = await context.newPage();
 
-  await page.goto(`${process.env.APP_URL}/twitter-embed/${id}`);
-  const buffer = await page
-    .locator(".react-tweet-theme")
-    .screenshot({ timeout: 5000 });
-  await browser.close();
+  // await page.goto(`${process.env.APP_URL}/twitter-embed/${id}`);
+  // const buffer = await page
+  //   .locator(".react-tweet-theme")
+  //   .screenshot({ timeout: 5000 });
+  // await browser.close();
 
   res.setHeader("Content-Type", "image/png");
-  res.end(buffer);
+  res.end("");
 };
 
 export default twitterEmbedEndpoint;
