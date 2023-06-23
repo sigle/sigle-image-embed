ed7952d44e7d0970c9c3f5ee7a929a343c056206
Léo Pradel <pradel.leo@gmail.com>
2023-06-23 17:04:27 +0200

    feat: first version

---
+++ src/app/twitter-embed/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { Tweet } from 'react-tweet';
+
+export const revalidate = 86400;
+
+type Props = {
+  params: { id: string };
+};
+
+/**
+ * We generate tweets on a separate page so they can then be screenshotted, and inserted as images
+ * in the emails sent to subscribers.
+ */
+export default function Page({ params }: Props) {
+  return (
+    <div data-theme="light">
+      <Tweet id={params.id} />
+    </div>
+  );
+}
---
+++ src/pages/api/twitter/[id].ts
@@ -0,0 +1,37 @@
+import { chromium as playwright } from "playwright-core";
+import chromium from "@sparticuz/chromium";
+import type { NextApiHandler } from "next";
+
+/**
+ * Generate a screenshot of a tweet using Playwright
+ */
+
+// Cache for 1 day
+export const revalidate = 86400;
+
+const twitterEmbedEndpoint: NextApiHandler = async (req, res) => {
+  const { id } = req.query;
+
+  const browser = await playwright.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? await chromium.executablePath
+        : undefined,
+    headless: true,
+  });
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto(`${process.env.APP_URL}/twitter-embed/${id}`);
+  const buffer = await page
+    .locator(".react-tweet-theme")
+    .screenshot({ timeout: 5000 });
+  await browser.close();
+
+  res.setHeader("Content-Type", "image/png");
+  res.end(buffer);
+};
+
+export default twitterEmbedEndpoint;
